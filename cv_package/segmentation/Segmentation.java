9c2350af3969701e790b8a8496cae0a60e29d72a
hsibayan <hannahsibayan@gmail.com>
2017-03-05 10:29:59 +0800

    Revert "Minor Change in Text Seg"
    This reverts commit 7e7f5d67173499f3a5f808f08efe1c7cb4fd371d.

--- cv_package/segmentation/Segmentation.java
+++ cv_package/segmentation/Segmentation.java
@@ -45,7 +45,7 @@ public class Segmentation {
     public static Segmentation getInstance() { return segmenter; }
     private Segmentation() { }
 	
-    public List segment(Form form) {
+    public void segment(Form form) {
     	List<MatOfPoint> groupContours;
     	Mat paperImage = form.getImage();
     	
@@ -68,13 +68,11 @@ public class Segmentation {
 		
 		int size = groupImages.size();
 				
-		List<List<Mat>> segmentedImages = new ArrayList<>();
-		
 		for(int i = 0; i < size; i++) {
 			
 			switch(groupTypes[i]) {
 				case FIELDTYPE_TEXT: 
-					segmentedImages.add(textSeg.segment(groupImages.get(i).clone(), form, i));
+					textSeg.segment(groupImages.get(i).clone(), form, i);
 					System.out.println("     [OK] Group # " + i + " SEGMENTATION: Letters Good");
 					break;
 				case FIELDTYPE_MARK: 
@@ -82,8 +80,6 @@ public class Segmentation {
 				case FIELDTYPE_BLOB:
 			}
 		}
-		
-		return segmentedImages;
 	}
     
     public List<MatOfPoint> filterGroups(List<MatOfPoint> contours, int elementCount) {
