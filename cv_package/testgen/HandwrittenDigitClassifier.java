b32fa27487c77a1833ddb266b3bb4f639f8004d1
hsibayan <hannahsibayan@gmail.com>
2017-06-30 13:21:28 +0800

    Add Model

--- cv_package/testgen/HandwrittenDigitClassifier.java
+++ cv_package/testgen/HandwrittenDigitClassifier.java
@@ -16,7 +16,8 @@ public class HandwrittenDigitClassifier {
 	
 	private MultiLayerNetwork model;
 	private NativeImageLoader loader;
-	private String modelLoc = "trained_mnist_model.zip";
+//	private String modelLoc = "trained_mnist_model.zip";
+	private String modelLoc = "src/cv_package/_model/trained_mnist_model.zip";
 	private List<Integer> labelList = Arrays.asList(0,1,2,3,4,5,6,7,8,9);
 	
 	public static void main(String args[]) throws Exception {
